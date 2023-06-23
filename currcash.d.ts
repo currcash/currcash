68972f34ec490e3ed1e2c629c44827a02a210cff
Moises Reyes <meelight12@gmail.com>
2023-06-23 11:17:11 -0400

    release v1.0.5 | updated currcash.d.ts

--- currcash.d.ts
+++ currcash.d.ts
@@ -8,7 +8,7 @@ export type Currency = {
 export declare class Currcash {
   public value?: number;
   public precision?: number;
-  public separators?: string[]
+  public separators?: string[];
 
   constructor({
     value = 0,
@@ -24,59 +24,30 @@ export declare class Currcash {
   public getPrecision(): number;
   public getSeparators(): string[];
 
-  /** Get value of currency */
   public val(): number;
-
-  /**  Add a value */
   public add(value?: number): this;
-
-  /** Subtract a value */
   public sub(value?: number): this;
-
-  /** Multiply a value */
   public multiply(value?: number): this;
-
-  /** Divide by a value */
   public divideBy(value?: number): this;
-
-  /** Check if the value is integer */
   public isInteger(): boolean;
-
-  /** Check if the value is decimal */
   public isDecimal(): boolean;
-
-  /** Check if the value is integer */
   static isInteger(value: number): boolean;
-
-  /** Check if the value is decimal */
   static isDecimal(value: number): boolean;
 }
 
 // ___ $ Exports ___
 export declare class CurrcashError extends Error {
-  public errorType: string
+  public errorType: string;
 
-  constructor(message: string)
+  constructor(message: string);
 }
 
 // ___ $ Modules: Operations ___
-
-/** Add each value in the array */
 export declare function add(values: number[]): number;
-
-/** Subtract each value in the array */
 export declare function sub(values: number[]): number;
-
-/** Multiply each value in the array */
 export declare function multiply(values: number[]): number;
-
-/** Divide each value in the array */
 export declare function divide(values: number[]): number;
 
 // ___ $ Modules: boolean ___
-
-/** Check if the value is integer */
 export declare function isInteger(value: number): boolean;
-
-/** Check if the value is decimal */
 export declare function isDecimal(value: number): boolean;
